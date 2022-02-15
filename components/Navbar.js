a16e3185ecaccd20c73107452e3ae31a6b4c795a
Aashiq420 <adams.aashiq@gmail.com>
2022-02-15 17:04:08 +0200

    setup project

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/logo.png" alt="site logo" width={117} height={53} />
+        {/* <h1>Ash News</h1> */}
+      </div>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/top-headlines"><a>Top Headlines</a></Link>
+      <Link href="/sport"><a>Sport</a></Link>
+      <Link href="/ninjas"><a>Business</a></Link>
+    </nav>
+  );
+}
+ 
+export default Navbar;
---
+++ pages/404.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(-1)
+      // router.go(1)
+      router.push('/')
+    }, 5000)
+  }, [])//empty array here for useEffect lets function only fire on 1st pageload
+
+  return (
+    <div className="not-found">
+      <h1>Ooops...</h1>
+      <h2>That page cannot be found :(</h2>
+      <p>Going back to the <Link href="/"><a>Homepage</a></Link> in 5 seconds...</p>
+    </div>
+  );
+}
+ 
+export default NotFound;
